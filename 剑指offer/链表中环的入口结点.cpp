d78275d9872bd2cfa98f1105bc8d85030cd231e9
YunaLin <linzy28@mail2.sysu.edu.cn>
2018-09-23 17:40:29 +0800

    链表中环的入口结点

--- 剑指offer/链表中环的入口结点.cpp
+++ 剑指offer/链表中环的入口结点.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <map>
+using namespace std;
 struct ListNode {
     int val;
     struct ListNode *next;
@@ -5,8 +8,27 @@ struct ListNode {
         val(x), next(NULL) {
     }
 };
-
 ListNode* EntryNodeOfLoop(ListNode* pHead) {
     if (pHead == NULL) return NULL;
-    
+    map<ListNode*, int> mymap;
+    ListNode* temp = pHead;
+    int count = 0;
+    while (temp != NULL && count < 100) {
+        mymap.insert(pair<ListNode*, int>(temp, 0));
+        temp = temp->next;
+        count++;
+    }
+    temp = pHead;
+    while (temp != NULL) {
+        if (mymap[temp] == 1) {
+            return temp;
+        } else {
+            mymap[temp] = 1;
+        }
+        temp = temp->next;
+    }
+    return NULL;
+}
+int main() {
+    return 0;
 }
